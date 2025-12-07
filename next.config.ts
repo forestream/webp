1c4a593b12259ebd0da70b942c510a78817509f2
hanbin <rj2303@naver.com>
2025-12-07 10:48:13 +0900

    Update packages

--- next.config.ts
+++ next.config.ts
@@ -1,20 +1,6 @@
 import { DevupUI } from "@devup-ui/next-plugin";
 import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = {
-  webpack(config, { isServer, dev }) {
-    // Use the client static directory in the server bundle and prod mode
-    // Fixes `Error occurred prerendering page "/"`
-    config.output.webassemblyModuleFilename =
-      isServer && !dev
-        ? "../static/wasm/[modulehash].wasm"
-        : "static/wasm/[modulehash].wasm";
-
-    // Since Webpack 5 doesn't enable WebAssembly by default, we should do it manually
-    config.experiments = { ...config.experiments, asyncWebAssembly: true };
-
-    return config;
-  },
-};
+const nextConfig: NextConfig = {};
 
 export default DevupUI(nextConfig);
